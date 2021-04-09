f08156bfd27c9a5ca157d3708592ffc40cf4942c
IMightBeMe <>
2021-04-09 15:09:50 +0000

    Updated main.py

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Scythe - A programming language
 
-Scythe is a new, (not) cutting edge programming language made in `Python` which is about 1 million times slower than C. Very Kool.
+Scythe is a new, powerful language developed by @IMightBeMe and @IMayBeMe which is written completly in `Python`. It has a wide range of capibilities and supports multiple different data types just as you would excpect in and language.
--- main.py
+++ main.py
@@ -2,93 +2,116 @@ from sly import Lexer, Parser
 
 
 class SLexer(Lexer):
-    tokens = {ID, NUMBER}
-    ignore = '\r \t'
+	tokens = {ID, NUMBER, STRING}
+	ignore = '\r \t'
+	ignore_comment = r'\#.*'
+	# ignore_newline = r'\n+'
+	literals = {'=', '+', '-', '*', '/', '(', ')'}
 
-    ignore_comment = r'\//.*'
-    # ignore_newline = r'\n+'
-    literals = {'=', '+', '-', '*', '/', '(', ')'}
+	# Tokens
+	ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-    # Tokens
-    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+	@_(r'\d+')
+	def NUMBER(self, t):
+		t.value = int(t.value)
+		return t
 
-    @_(r'\d+')
-    def NUMBER(self, t):
-        t.value = int(t.value)
-        return t
+	@_(r'\d+')
+	def NUMBER(self, t):
+		t.value = int(t.value)
+		return t
 
-    @_(r'\n+')
-    def newline(self, t):
-        self.lineno += t.value.count('\n')
+	@_(r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
+	def STRING(self, t):
+		lst = [char for char in t.value]
+		del lst[0]
+		del lst[len(lst) - 1]
+		t.value = ''.join(lst)
+		return t
 
-    def error(self, t):
-        print(f'Illegal character {t.value[0]} on line {self.lineno}')
-        self.index += 1
+	@_(r'\n+')
+	def newline(self, t):
+		self.lineno += t.value.count('\n')
+
+	def error(self, t):
+		print(f'Illegal character {t.value[0]} on line {self.lineno}')
+		self.index += 1
 
 
 class SParser(Parser):
-    tokens = SLexer.tokens
+	tokens = SLexer.tokens
 
-    precedence = (
-        ('left', '+', '-'),
-        ('left', '*', '/'),
-        ('right', 'UMINUS'),
-    )
+	precedence = (
+		('left', '+', '-'),
+		('left', '*', '/'),
+		('right', 'UMINUS'),
+	)
 
-    def __init__(self):
-        self.names = {}
+	def __init__(self):
+		self.names = {}
 
-    @_('ID "=" expr')
-    def statement(self, p):
-        self.names[p.ID] = p.expr
+	@_('ID "=" expr')
+	def statement(self, p):
+		self.names[p.ID] = p.expr
 
-    @_('expr')
-    def statement(self, p):
-        print(p.expr)
+	@_('expr')
+	def statement(self, p):
+		print(p.expr)
 
-    @_('expr "+" expr')
-    def expr(self, p):
-        return p.expr0 + p.expr1
+	@_('expr "+" expr')
+	def expr(self, p):
+		try:
+			return p.expr0 + p.expr1
+		except TypeError:
+			return f'Unsupported data type \'+\' for {p.expr0} and {p.expr11} on line {self.lineno}'
 
-    @_('expr "-" expr')
-    def expr(self, p):
-        return p.expr0 - p.expr1
+	@_('expr "-" expr')
+	def expr(self, p):
+		try:
+			return p.expr0 - p.expr1
+		except TypeError:
+			return f'Unsupported data type \'-\' for {p.expr0} and {p.expr1} on line {self.lineno}'
 
-    @_('expr "*" expr')
-    def expr(self, p):
-        return p.expr0 * p.expr1
+	@_('expr "*" expr')
+	def expr(self, p):
+		return p.expr0 * p.expr1
 
-    @_('expr "/" expr')
-    def expr(self, p):
-        return p.expr0 / p.expr1
+	@_('expr "/" expr')
+	def expr(self, p):
+		return p.expr0 / p.expr1
 
-    @_('"-" expr %prec UMINUS')
-    def expr(self, p):
-        return -p.expr
+	@_('"-" expr %prec UMINUS')
+	def expr(self, p):
+		return -p.expr
 
-    @_('"(" expr ")"')
-    def expr(self, p):
-        return p.expr
+	@_('"(" expr ")"')
+	def expr(self, p):
+		return p.expr
 
-    @_('NUMBER')
-    def expr(self, p):
-        return p.NUMBER
+	@_('NUMBER')
+	def expr(self, p):
+		return p.NUMBER
 
-    @_('ID')
-    def expr(self, p):
-        try:
-            return self.names[p.ID]
-        except LookupError:
-            print(f'Undefined name {p.ID}')
-            return 0
+	@_('STRING')
+	def expr(self, p):
+		return p.STRING
+
+	@_('ID')
+	def expr(self, p):
+		try:
+			return self.names[p.ID]
+		except LookupError:
+			print(f'Undefined name {p.ID}')
+			return 0
 
 
 if __name__ == '__main__':
-    lexer = SLexer()
-    parser = SParser()
+	lexer = SLexer()
+	parser = SParser()
 
-    data = open('main.scy', 'r')
-    lines = data.readlines()
-    for i in lines:
-        if i:
-            parser.parse(lexer.tokenize(i))
+	scythe_file = open('main.scy', 'r')
+
+	lines = scythe_file.readlines()
+	for i in lines:
+		if i:
+			parser.parse(lexer.tokenize(i))
