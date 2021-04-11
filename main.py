6123694359901532bd6bea349fdf84f149e91503
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2021-04-11 12:27:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,125 @@
 from sly import Lexer, Parser
+import glob
 
 
 class SLexer(Lexer):
-	tokens = {ID, NUMBER, STRING}
-	ignore = '\r \t'
-	ignore_comment = r'\#.*'
-	# ignore_newline = r'\n+'
-	literals = {'=', '+', '-', '*', '/', '(', ')'}
+    tokens = {ID, NUMBER, STRING, SYOUT, DATATYPE}
+    ignore = '\r \t'
+    ignore_comment = r'\//.*'
+    literals = {'=', '+', '-', '*', '/', '(', ')'}
 
-	# Tokens
-	ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    # Tokens
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['syout'] = SYOUT
+    ID['datatype'] = DATATYPE
 
-	@_(r'\d+')
-	def NUMBER(self, t):
-		t.value = int(t.value)
-		return t
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
 
-	@_(r'\d+')
-	def NUMBER(self, t):
-		t.value = int(t.value)
-		return t
+    @_(r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
+    def STRING(self, t):
+        lst = [char for char in t.value]
+        del lst[0]
+        del lst[len(lst) - 1]
+        t.value = ''.join(lst)
+        return t
 
-	@_(r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
-	def STRING(self, t):
-		lst = [char for char in t.value]
-		del lst[0]
-		del lst[len(lst) - 1]
-		t.value = ''.join(lst)
-		return t
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno += t.value.count('\n')
 
-	@_(r'\n+')
-	def newline(self, t):
-		self.lineno += t.value.count('\n')
-
-	def error(self, t):
-		print(f'Illegal character {t.value[0]} on line {self.lineno}')
-		self.index += 1
+    def error(self, t):
+        print(f'Illegal character {t.value[0]} on line {self.lineno}')
+        self.index += 1
 
 
 class SParser(Parser):
-	tokens = SLexer.tokens
+    tokens = SLexer.tokens
 
-	precedence = (
-		('left', '+', '-'),
-		('left', '*', '/'),
-		('right', 'UMINUS'),
-	)
+    precedence = (
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS'),
+    )
 
-	def __init__(self):
-		self.names = {}
+    def __init__(self):
+        self.names = {}
 
-	@_('ID "=" expr')
-	def statement(self, p):
-		self.names[p.ID] = p.expr
+    @_('DATATYPE "(" expr ")" ')
+    def statment(self, p):
+        typ = str(type(p.expr))
+        typ = typ.replace('<class ', '')
+        typ = typ.replace('>', '')
+        typ = typ.replace('\'', '')
+        return typ
 
-	@_('expr')
-	def statement(self, p):
-		print(p.expr)
+    @_('SYOUT "(" expr ")" ')
+    def statement(self, p):
+        print(p.expr)
 
-	@_('expr "+" expr')
-	def expr(self, p):
-		try:
-			return p.expr0 + p.expr1
-		except TypeError:
-			return f'Unsupported data type \'+\' for {p.expr0} and {p.expr11} on line {self.lineno}'
+    @_('ID "=" expr')
+    def statement(self, p):
+        self.names[p.ID] = p.expr
 
-	@_('expr "-" expr')
-	def expr(self, p):
-		try:
-			return p.expr0 - p.expr1
-		except TypeError:
-			return f'Unsupported data type \'-\' for {p.expr0} and {p.expr1} on line {self.lineno}'
+    @_('expr')
+    def statement(self, p):
+        pass
 
-	@_('expr "*" expr')
-	def expr(self, p):
-		return p.expr0 * p.expr1
+    @_('expr "+" expr')
+    def expr(self, p):
+        try:
+            return p.expr0 + p.expr1
+        except TypeError:
+            return f'Unsupported data type \'+\' for {p.expr0} and {p.expr1}'
 
-	@_('expr "/" expr')
-	def expr(self, p):
-		return p.expr0 / p.expr1
+    @_('expr "-" expr')
+    def expr(self, p):
+        try:
+            return p.expr0 - p.expr1
+        except TypeError:
+            return f'Unsupported data type \'-\' for {p.expr0} and {p.expr1}'
 
-	@_('"-" expr %prec UMINUS')
-	def expr(self, p):
-		return -p.expr
+    @_('expr "*" expr')
+    def expr(self, p):
+        return p.expr0 * p.expr1
 
-	@_('"(" expr ")"')
-	def expr(self, p):
-		return p.expr
+    @_('expr "/" expr')
+    def expr(self, p):
+        return p.expr0 / p.expr1
 
-	@_('NUMBER')
-	def expr(self, p):
-		return p.NUMBER
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return -p.expr
 
-	@_('STRING')
-	def expr(self, p):
-		return p.STRING
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return p.expr
 
-	@_('ID')
-	def expr(self, p):
-		try:
-			return self.names[p.ID]
-		except LookupError:
-			print(f'Undefined name {p.ID}')
-			return 0
+    @_('NUMBER')
+    def expr(self, p):
+        return p.NUMBER
+
+    @_('STRING')
+    def expr(self, p):
+        return p.STRING
+
+    @_('ID')
+    def expr(self, p):
+        try:
+            return self.names[p.ID]
+        except LookupError:
+            return f'Undefined name {p.ID} '
 
 
 if __name__ == '__main__':
-	lexer = SLexer()
-	parser = SParser()
+    lexer = SLexer()
+    parser = SParser()
 
-	scythe_file = open('main.scy', 'r')
+    scythe_file = open('main.scy', 'r')
 
-	lines = scythe_file.readlines()
-	for i in lines:
-		if i:
-			parser.parse(lexer.tokenize(i))
+    lines = scythe_file.readlines()
+    for i in lines:
+        if i:
+            parser.parse(lexer.tokenize(i))
