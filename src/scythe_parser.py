f33cfe21bcb56055599a8783b27db150de86d758
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2021-04-28 16:15:35 -0400

    Adding less than or equal to and greater than or equal to (doesn't work yet)

--- src/scythe_parser.py
+++ src/scythe_parser.py
@@ -4,107 +4,108 @@ from scythe_lexer import SLexer
 
 
 class SParser(Parser):
-	tokens = SLexer.tokens
+    tokens = SLexer.tokens
 
-	precedence = (
-		('left', '+', '-'),
-		('left', '*', '/'),
-		('right', 'UMINUS', '<', '>', '<=', '>='),
-	)
+    precedence = (
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS', '<', '>'),
+        ('right', '<=', '>='),
+    )
 
-	def __init__(self):
-		self.names = {}
+    def __init__(self):
+        self.names = {}
 
-	@_('COMMENT')
-	def statement(self, p):
-		pass
+    @_('COMMENT')
+    def statement(self, p):
+        pass
 
-	@_('IF "(" expr ")" "{" statement "}" ELSE "{" statement "}" ')
-	def statement(self, p):
-		return ('compare', p.expr, p.statement0, p.statement1)
+    @_('IF "(" expr ")" "{" statement "}" ELSE "{" statement "}" ')
+    def statement(self, p):
+        return ('compare', p.expr, p.statement0, p.statement1)
 
-	@_('ID "=" expr')
-	def statement(self, p):
-		self.names[p.ID] = p.expr[1]
-		return ('var_assign', p.ID, p.expr)
+    @_('ID "=" expr')
+    def statement(self, p):
+        self.names[p.ID] = p.expr[1]
+        return ('var_assign', p.ID, p.expr)
 
-	@_('expr')
-	def statement(self, p):
-		return (p.expr)
+    @_('expr')
+    def statement(self, p):
+        return (p.expr)
 
-	@_('SYOUT "(" expr ")" ')
-	def statement(self, p):
-		return ('syout', p.expr)
+    @_('SYOUT "(" expr ")" ')
+    def statement(self, p):
+        return ('syout', p.expr)
 
-	@_('expr ">" expr')
-	def expr(self, p):
-		return ('greater', p.expr0, p.expr1)
+    @_('expr ">" expr')
+    def expr(self, p):
+        return ('greater', p.expr0, p.expr1)
 
-	@_('expr "<" expr')
-	def expr(self, p):
-		return ('less', p.expr0, p.expr1)
+    @_('expr "<" expr')
+    def expr(self, p):
+        return ('less', p.expr0, p.expr1)
 
-	@_('expr "<=" expr')
-	def expr(self, p):
-		return ('less_or_equal', p.expr0, p.expr1)
+    @_('expr "<=" expr')
+    def expr(self, p):
+        return ('less_or_equal', p.expr0, p.expr1)
 
-	@_('expr ">=" expr')
-	def expr(self, p):
-		return ('greater_or_equal', p.expr0, p.expr1)
+    @_('expr ">=" expr')
+    def expr(self, p):
+        return ('greater_or_equal', p.expr0, p.expr1)
 
-	@_('DATATYPE "(" expr ")" ')
-	def expr(self, p):
-		return ('datatype', p.expr)
+    @_('DATATYPE "(" expr ")" ')
+    def expr(self, p):
+        return ('datatype', p.expr)
 
-	@_('INT "(" expr ")" ')
-	def expr(self, p):
-		return ('int_con', p.expr)
+    @_('INT "(" expr ")" ')
+    def expr(self, p):
+        return ('int_con', p.expr)
 
-	@_('STR "(" expr ")" ')
-	def expr(self, p):
-		return ('str_con', p.expr)
+    @_('STR "(" expr ")" ')
+    def expr(self, p):
+        return ('str_con', p.expr)
 
-	@_('expr "+" expr')
-	def expr(self, p):
-		try:
-			return ('add', p.expr0, p.expr1)
-		except TypeError:
-			return f'Unsupported data type \'+\' for {p.expr0} and {p.expr1}'
+    @_('expr "+" expr')
+    def expr(self, p):
+        try:
+            return ('add', p.expr0, p.expr1)
+        except TypeError:
+            return f'Unsupported data type \'+\' for {p.expr0} and {p.expr1}'
 
-	@_('expr "-" expr ')
-	def expr(self, p):
-		try:
-			return ('sub', p.expr0, p.expr1)
-		except TypeError:
-			return f'Unsupported data type \'-\' for {p.expr0} and {p.expr1}'
+    @_('expr "-" expr ')
+    def expr(self, p):
+        try:
+            return ('sub', p.expr0, p.expr1)
+        except TypeError:
+            return f'Unsupported data type \'-\' for {p.expr0} and {p.expr1}'
 
-	@_('expr "*" expr ')
-	def expr(self, p):
-		return ('times', p.expr0, p.expr1)
+    @_('expr "*" expr ')
+    def expr(self, p):
+        return ('times', p.expr0, p.expr1)
 
-	@_('expr "/" expr ')
-	def expr(self, p):
-		return ('div', p.expr0, p.expr1)
+    @_('expr "/" expr ')
+    def expr(self, p):
+        return ('div', p.expr0, p.expr1)
 
-	@_('"-" expr %prec UMINUS')
-	def expr(self, p):
-		return p.expr
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return p.expr
 
-	@_('"(" expr ")"')
-	def expr(self, p):
-		return (p.expr)
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return (p.expr)
 
-	@_('NUMBER')
-	def expr(self, p):
-		return ('num', p.NUMBER)
+    @_('NUMBER')
+    def expr(self, p):
+        return ('num', p.NUMBER)
 
-	@_('STRING')
-	def expr(self, p):
-		return ('string', p.STRING)
+    @_('STRING')
+    def expr(self, p):
+        return ('string', p.STRING)
 
-	@_('ID')
-	def expr(self, p):
-		try:
-			return ('var', p.ID)
-		except:
-			return f'Undefined name {p.ID}'
+    @_('ID')
+    def expr(self, p):
+        try:
+            return ('var', p.ID)
+        except:
+            return f'Undefined name {p.ID}'
